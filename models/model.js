f133fc9e3ff32774d689b172bcdab46fb3ae1ea1
Roman Sorokin <mail@r1000.ru>
2019-09-19 16:48:39 +0300

    init

---
+++ models/model.js
@@ -0,0 +1,17 @@
+const CarModel = function() {
+    
+}
+
+CarModel.prototype.setColor = function(r, g, b) {
+
+}
+
+CarModel.prototype.setCoords = function(lat, lon, speed) {
+
+}
+
+CarModel.prototype.setParams = function(params) {
+
+}
+
+export {CarModel}
